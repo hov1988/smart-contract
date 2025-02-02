45e067e2442674356fbe269191033325c46e479d
hov1988 <hhov88@gmail.com>
2025-02-02 13:53:41 +0400

    Add buy and sell functions

--- hadcoin_ico.sol
+++ hadcoin_ico.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.21;
+pragma solidity ^0.8.21;
 
 contract hadcoin_ico {
     // ICO info 
@@ -16,11 +16,25 @@ contract hadcoin_ico {
         _;
     }
 
-    function equity_in_hadcoins(address investor) external constant returns (uint) {
-        return equity_hadcoins[address];
+    function equity_in_hadcoins(address investor) external view returns (uint) {
+        return equity_hadcoins[investor];
     }
 
-    function equity_in_usd(address investor) external constant returns (uint) {
-        return equity_hadcoins[address];
+    function equity_in_usd(address investor) external view returns (uint) {
+        return equity_usd[investor];
     }
+
+    function buy_hadcoin(address investor, uint usd_investor) external 
+    can_buy_hadcoin(usd_investor) {
+        uint hadcoun_bought = usd_investor * usd_to_hadcoins;
+        equity_hadcoins[investor] += hadcoun_bought;
+        equity_usd[investor] += equity_hadcoins[investor] / 1000;
+        total_hadcoint_bought += hadcoun_bought;
+    }
+
+   function sell_hadcoin(address investor, uint sold_hadcoins) external {
+        equity_hadcoins[investor] -= sold_hadcoins;
+        equity_usd[investor] += equity_hadcoins[investor] / 1000;
+        total_hadcoint_bought -= sold_hadcoins;
+   }
 }
